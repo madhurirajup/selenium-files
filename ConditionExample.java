6f548c19474787081343582093da51b076a37277
madhurirajup <madhurirajup@gmail.com>
2016-11-15 11:23:45 -0800

    Add files via upload

---
+++ ConditionExample.java
@@ -0,0 +1,26 @@
+package madhu;
+
+public class ConditionExample 
+{
+
+	public static void main(String[] args)
+	{
+		int a = 10; 
+		int b = 20;
+		
+		if (a>b)
+		{
+			System.out.println(" A is greater");
+		
+		}
+		
+		else
+		
+		{
+		System.out.println(" B is greater");
+		}
+		
+
+	}
+
+}
